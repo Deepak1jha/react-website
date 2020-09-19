3e5904233f34e862b1311a7932bc8b4e1f4ea615
Deepak1jha <ddjha171@gmail.com>
2020-09-19 12:34:24 +0530

    Updates

---
+++ precache-manifest.c2cb64ac4f5b5c45c9f2ea10343f75a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c657729a99ed7f9e390c9c20d379a54d",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "b3342303c3213c4df195",
+    "url": "/react-website/static/css/main.4f5fc301.chunk.css"
+  },
+  {
+    "revision": "0525de3e995c297c7fa8",
+    "url": "/react-website/static/js/2.c466e38b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-website/static/js/2.c466e38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3342303c3213c4df195",
+    "url": "/react-website/static/js/main.9e20f6b0.chunk.js"
+  },
+  {
+    "revision": "0fc6e45962770e50fdfd",
+    "url": "/react-website/static/js/runtime-main.8dc8c645.js"
+  }
+]);
