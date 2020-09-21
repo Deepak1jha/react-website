be27d1f0dfe36dbee395382767a6975485d059b6
Deepak1jha <ddjha171@gmail.com>
2020-09-21 10:21:12 +0530

    IMage url on cloud

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -10,13 +10,13 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
+              src='https://firebasestorage.googleapis.com/v0/b/isstracker-190613.appspot.com/o/react-simple-site%2Fimg-9.jpg?alt=media&token=04cd8fe5-bf12-449a-95ab-036b7c17625e'
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
+              src='https://firebasestorage.googleapis.com/v0/b/isstracker-190613.appspot.com/o/react-simple-site%2Fimg-2.jpg?alt=media&token=4ce8bda3-bb1e-4fa1-85c2-4d0b27529968'
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src='https://firebasestorage.googleapis.com/v0/b/isstracker-190613.appspot.com/o/react-simple-site%2Fimg-3.jpg?alt=media&token=f1d34ba3-f98b-459d-a663-39a9e3acb94a'
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
+              src='https://firebasestorage.googleapis.com/v0/b/isstracker-190613.appspot.com/o/react-simple-site%2Fimg-4.jpg?alt=media&token=a108b228-39cd-476b-a70c-fb94aeff7626'
+              text='Experience Football on Top of the Himalayan Mountains'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src='https://firebasestorage.googleapis.com/v0/b/isstracker-190613.appspot.com/o/react-simple-site%2Fimg-8.jpg?alt=media&token=df6d4a65-7243-4128-906c-dc00cf76429f'
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
